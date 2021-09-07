9fffb2a58336eaaf1877815159b24203b830b66a
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-09-07 18:33:30 +0200

    have been added user and premiumUser

--- index.php
+++ index.php
@@ -7,12 +7,14 @@
 //  oppure diverse tipologie di prodotti.
 
 
-//1. creiamo l'eshop
+// creiamo l'eshop
 class EShop {
     
     public $name;
     public $address; 
     public $products=[];
+    public $users =[];
+    public $premiumUsers=[];
 
     public function __construct(string $nameEShop, string $addressEShop)
 
@@ -21,15 +23,23 @@ class EShop {
       $this ->address = $addressEShop;
 
     }
-
+    // mettiamo una lista di prodotti
     public function addProducts(Product $nomeProduct){
 
         $this ->products[]=$nomeProduct;
     }
     public function getProducts(){
+        echo $this -> products[0]->label;
         return $this ->products;
     }
-    // mettiamo una lista di prodotti
+   // mettiamo una lista di users 
+   public function addUsers(User $users){
+       $this ->users[]=$users;
+   }
+    // mettiamo una lista di premiumUsers 
+   public function addPremiumUsers( PremiumUser $premiumUsers){
+    $this ->premiumUsers[]=$premiumUsers;
+}
 
 }
 
@@ -37,9 +47,9 @@ class EShop {
 
 class Product {
  
-    protected $label;
-    protected $price;
-    protected  $type;
+    public $label;
+    public $price;
+    public  $type;
 
     public function getLabel() {
         return $this->label;
@@ -54,11 +64,11 @@ class Product {
 
 }
 
-//2. creiamo diversi prodotti
+//  creiamo diversi prodotti
 class TechProduct extends Product {
      // qui inseriremo solo le proprietà relative alla TechProduct 
 
-    public function __construct(string $labelProduct, string $typeProduct, numeric $priceProduct )
+    public function __construct(string $labelProduct, string $typeProduct, int $priceProduct )
     {
       $this->type='Tech';
       $this ->label =$labelProduct;
@@ -70,7 +80,7 @@ class TechProduct extends Product {
 
 class BeautyProduct extends Product {
      // qui inseriremo solo le proprietà relative alla BeautyProduct
-     public function __construct(string $labelProduct, string $typeProduct, numeric $priceProduct )
+     public function __construct(string $labelProduct, string $typeProduct, int $priceProduct )
      {
        $this->type='Beauty';
        $this ->label =$labelProduct;
@@ -82,36 +92,70 @@ class BeautyProduct extends Product {
 
 
 // ---------------------------------------------
-
+//creaiamo l'utente normale
 class User {
     public $nome;
     public $cognome;
     public $email;
     public $number;
+    public $address;
     public $sconto = 0;
+    public function __construct(string $nome, string $cognome, string $email, int $number, string $address)
+
+    {
+      $this->name = $nome;
+      $this ->cognome = $cognome;
+      $this ->email =$email;
+      $this ->number =$number;
+    }
 
 }
-
+// creiamo un utente premium
 class PremiumUser {
     public $nome;
     public $cognome;
     public $email;
     public $number;
+    public $address;
+    public $creditCard  =[];
     // qui dentro potrebbe avere la definizione di una percentuale di 
     // sconto per ogni prodotto.
     public $sconto = 50;
 
+    public function __construct(string $nome, string $cognome, string $email, int $number, string $address)
+
+    {
+      $this->name = $nome;
+      $this ->cognome = $cognome;
+      $this ->email =$email;
+      $this ->number =$number;
+    }
+
+    // public function getCreditCard(CreditCard $creditCard){
+
+    // }
 
 }
+//inseriamo le carte di credito per ciascun utente
+class CreditCard{
+
+ public $name;
+ public $term;
+ public function getName() {
+    return $this->name;
+}
+
+public function getTerm() {
+    return $this->term;
+}
+}
+
 
 // ----------------------------------------------
-
-
 /*
-3. aggiungiamoli all'eshop
-4. creaiamo l'utente normale
-5. creiamo un utente premium
-6. inseriamo le carte di credito per ciascun utente
+
+
+
 6. l'utente normale acquista un prodotto
 7. l'utente premium acquista un altro prodotto (e riceve lo sconto)
 */
@@ -120,11 +164,15 @@ $eShop = new EShop('Amazon', 'Via della Magliana, 375, 00148 Roma RM');
 var_dump($eShop);
 ?>
 <hr>
+
 <?php
-$mascara = new Product('Chanel', 'Beauty', 50);
-$cellulare = new Product('Samsung', 'Tech', 400);
-$tastiera = new Product('hp', 'Tech', 20);
-$matita = new Product('Kiko', 'Beauty', 6);
+
+// aggiungiamoli all'eshop i prodotti
+
+$mascara = new BeautyProduct('Chanel', 'Beauty', 50);
+$cellulare = new TechProduct('Samsung', 'Tech', 400);
+$tastiera = new TechProduct('hp', 'Tech', 20);
+$matita = new BeautyProduct('Kiko', 'Beauty', 6);
 
 $eShop ->addProducts($mascara);
 $eShop ->addProducts($matita);
@@ -132,5 +180,16 @@ $eShop ->addProducts($cellulare);
 $eShop ->addProducts($tastiera);
 
 var_dump($eShop -> getProducts());
+?>
+<br>
+<br><hr>
+<?php
+$cardUser = new CreditCard();
+
+$client1 = new User('Perizat', 'Shatyrbekova', 'sdcwecw@gmail.com', 333345533, 'Via Ferdinando Acton 21');
+$client2 = new PremiumUser('Pippo', 'Baudi', 'pippo.baudi@gmail.com', 3313121312, 'Piazza Cavour 155');
+
+var_dump($client1);
+var_dump($client2);
 
 ?>
